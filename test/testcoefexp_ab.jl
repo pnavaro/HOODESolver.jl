4101fa515eda556f6a93de21a7fbea6aeb7eebdd
Yves Mocquard <yves.mocquard@irisa.fr>
2020-02-04 16:45:44 +0100

    Rtest for coefexp

--- test/testcoefexp_ab.jl
+++ test/testcoefexp_ab.jl
@@ -1,9 +1,15 @@
 include("../src/coefexp_ab.jl")
+include("datacoefexp.jl")
 using Test
-
-
 function testcoefexp_ab()
     
-    par = CoefExpAB(15, big"0.1", )
+    par = CoefExpAB(15, big"0.1", 32, big"0.0001")
 
-    @test 
+    tab = get_coef_ab_for_test()
+
+    println("norm = $(norm(par.tab_coef-tab))")
+
+    @test approx.(par.tab_coef, tab, atol=1e-70, rtol=1e-70)
+end
+
+testcoefexp_ab()
