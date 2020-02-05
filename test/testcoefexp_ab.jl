1c5d16c842ffc551c36f5daac2d21eda62334b1a
Yves Mocquard <yves.mocquard@irisa.fr>
2020-02-05 09:55:59 +0100

    mall bug

--- test/testcoefexp_ab.jl
+++ test/testcoefexp_ab.jl
@@ -6,7 +6,7 @@ function testcoefexp_ab()
     par = CoefExpAB(15, big"0.1", 32, big"0.0001")
     tab, list_j = get_coef_ab_for_test()
     @time @testset "test coef for exponential Adams-Bashforth" begin
-        for i in list_j
+        for j in list_j
             println("norm = $(norm(par.tab_coef[:,:,j]-tab[:,:,j], Inf))")
             @time @test all(isapprox.(par.tab_coef[:,:,j], tab[:,:,j], atol=1e-76, rtol=1e-76))
         end
