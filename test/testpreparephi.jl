579497b4e578dac87c597afe19646adcbf22fef2
Yves Mocquard <yves.mocquard@irisa.fr>
2020-02-10 17:23:39 +0100

    testprepaprephi.jl is good !!!!

--- test/testpreparephi.jl
+++ test/testpreparephi.jl
@@ -1,12 +1,28 @@
-
+include("../src/preparephi.jl")
+include("../src/henon_heiles.jl")
+include("dataprep_u0.jl")
+using Test
 function testpreparephi()
+    @time @testset "phi and prepare u0" begin
 
-    @time @testset "test 2 exp big matrices" begin
-
-    a faire
-
+        epsilon = big"1"/256
+        n_tau = 32
+        parphi = PreparePhi(
+    epsilon, 
+    n_tau, 
+    [0 0 1 0; 0 0 0 0; -1 0 0 0; 0 0 0 0],
+    henon_heiles
+)
+        tab_ref = undef
+        setprecision(1024) do
+            tab_ref = get_prepare_u0_for_test()
+        end
+        u0 = BigFloat.([0.125, 0.140625, 0.15625, 0.171875])
+        for ord=2:10
+            paru0 = PrepareU0(parphi, ord, u0, 1024)
+            println("norm=$(norm(tab_ref[:, :, ord]- paru0.ut0))")
+            @test isapprox(tab_ref[:, :, ord], paru0.ut0, atol=1e-77, rtol=1e-77)
+        end
     end
-
 end
-
 testpreparephi()
