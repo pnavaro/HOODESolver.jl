e18b3bf13a687131b76981744ca33f85f12971d5
Yves Mocquard <yves.mocquard@irisa.fr>
2020-02-13 16:50:12 +0100

    refining gen data test

--- test/testpreparephi.jl
+++ test/testpreparephi.jl
@@ -89,27 +89,29 @@ end
 
 function testpreparephi()
     @time @testset "phi and prepare u0" begin
-        tab_ref = setprecision(1024) do
+        tab_ref, tab_eps = setprecision(1024) do
             get_prepare_u0_for_test()
         end
         for i_prec=1:3
             prec = 256*2^(i_prec-1)
             tol=max((1e-77)^i_prec, 1e-200)
             setprecision(prec) do
-                epsilon = big"1"/256
-                n_tau = 32
-                parphi = PreparePhi(
+                for i_prec=1:size(tab_eps,1)
+                   epsilon = tab_eps[i_prec]
+                   n_tau = 32
+                   parphi = PreparePhi(
     epsilon, 
     n_tau, 
     [0 0 1 0; 0 0 0 0; -1 0 0 0; 0 0 0 0],
     henon_heiles
 )
-                u0 = BigFloat.([0.125, 0.140625, 0.15625, 0.171875])
-                for ord=2:10
+                    u0 = BigFloat.([0.125, 0.140625, 0.15625, 0.171875])
+                    for ord=2:10
             #     paru0 = PrepareU0(parphi, ord, u0, 1024)
-                    paru0 = PrepareU0(parphi, ord, u0)
+                        paru0 = PrepareU0(parphi, ord, u0)
  #                   println("prec=$prec ord=$ord norm=$(norm(tab_ref[:, :, ord]- paru0.ut0))")
-                    @test isapprox(tab_ref[:, :, ord], paru0.ut0, atol=tol, rtol=tol)
+                        @test isapprox(tab_ref[:, :, ord, i_prec], paru0.ut0, atol=tol, rtol=tol)
+                    end
                 end
             end
         end
