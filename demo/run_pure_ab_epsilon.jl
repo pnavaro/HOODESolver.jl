cf0801dd6818b9036130c09cbaf4a4e4a2948e5a
Yves Mocquard <yves.mocquard@irisa.fr>
2020-02-18 15:18:05 +0100

    some bugs

--- demo/run_pure_ab_epsilon.jl
+++ demo/run_pure_ab_epsilon.jl
@@ -22,40 +22,29 @@ end
 function fctMain(n_tau)
 
     u0 =[big"0.12345678", big"0.1209182736", big"0.1290582671", big"0.1239681094" ]
-
-    Random.seed!(123456)
-
+    seed=123456
+    Random.seed!(seed)
     B = 2rand(BigFloat, 4, 4) - ones(BigFloat, 4, 4)
-    println("B=$B")
-
+    println("seed = $seed B=$B")
     tab_eps = zeros(BigFloat,8)
     epsilon=big"0.1"
     for i=1:8
         tab_eps[i] = epsilon
-        eps /= 10
+        epsilon /= 10
     end
-
-#    tab_eps=[big"0.005"]
     nbMaxTest=8
     order=6
     t_max = big"1.0"
- #    y = ones(Float64, nbMaxTest, ordmax-debord+1 )
     y = ones(Float64, nbMaxTest, size(tab_eps,1) )
     x=zeros(Float64,nbMaxTest)
-
     ind=1
     for epsilon in tab_eps
-  #      @time solref = juliaStdSolve(henonHeiles, u0, big"1.0", eps, 1e-40, 1e-40 )
-
         fct = u -> B*u
-        parphi = PreparePhi(n_tau, epsilon, [0 0 1 0; 0 0 0 0;-1 0 0 0; 0 0 0 0], fct, B)
+        parphi = PreparePhi(epsilon, n_tau, [0 0 1 0; 0 0 0 0;-1 0 0 0; 0 0 0 0], fct, B)
         println("prepareU0 eps=$epsilon n_tau=$n_tau")
         @time par_u0 = PrepareU0(parphi, 2, u0)
-
         solref = getexactsol(parphi, u0, t_max)
-
         eps_v = convert(Float32,epsilon)
-
         println("epsilon = $eps_v solref=$solref")
         nb = 100
         indc =1
