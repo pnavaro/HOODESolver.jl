e81f3d7619d2e66ecd0408ddde03cc8f74fa37ae
Yves Mocquard <yves.mocquard@irisa.fr>
2020-03-02 12:24:39 +0100

    new test

---
+++ test/testprepall.jl
@@ -0,0 +1,28 @@
+include("../src/preparephi.jl")
+include("../src/henon_heiles.jl")
+using Test
+
+function testprepare()
+    n_tau=32
+    A = [0 0 1 0; 0 0 0 0;-1 0 0 0; 0 0 0 0]
+    u =rand(BigFloat, 4)
+    for i=1:5
+        prec = 256*2^(i-1)
+        setprecision(256*2^(i-1))
+        for i_eps = 1:20
+            epsilon = 1.25^(-i_eps)
+            @time @testset "test PrepareU0 prec=$prec epsilon=$epsilon" begin
+                for order=3:16
+                    newprec = prec + 32 + div(-exponent(epsilon)*order^2, 3)
+                    precref = newprec + 10
+                    parphi = PreparePhi(epsilon, n_tau, A, henon_heiles)
+                    pref=PrepareU0(parphi, order, u, precref)
+                    pnew=PrepareU0(parphi, order, u, newprec)
+                    @test pref.ut0 == pnew.ut0
+                end
+            end
+        end
+    end
+
+
+end
