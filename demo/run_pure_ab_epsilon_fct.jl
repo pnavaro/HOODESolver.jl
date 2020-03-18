9045c844f25e083df455148464b7cdf9cf6be98f
Yves Mocquard <yves.mocquard@irisa.fr>
2020-03-18 11:29:41 +0100

    simplify graphe

--- demo/run_pure_ab_epsilon_fct.jl
+++ demo/run_pure_ab_epsilon_fct.jl
@@ -41,7 +41,8 @@ function fctMain(n_tau)
     u0=rand(BigFloat,4)
     println("seed = $seed")
     tab_eps = zeros(BigFloat,7)
-    tab_eps= [big"0.5", big"0.2", big"0.1",big"0.05", big"0.02", big"0.01",big"0.005", big"0.002", big"0.001",big"0.0005", big"0.0002", big"0.0001"]
+ #   tab_eps= [big"0.5", big"0.2", big"0.1",big"0.05", big"0.02", big"0.01",big"0.005", big"0.002", big"0.001",big"0.0005", big"0.0002", big"0.0001"]
+    tab_eps= [big"0.5",big"0.05", big"0.005",big"0.0005", big"0.00005"]
     # epsilon=big"0.1"
     # for i=1:7
     #     tab_eps[i] = epsilon
@@ -122,13 +123,13 @@ function fctMain(n_tau)
             xaxis=:log,
             ylabel="error",
             yaxis=:log,
-            legend=:topleft,
+            legend=:bottomright,
             label=labels,
             marker=2
         )
                 
                 prec_v = precision(BigFloat)
-                Plots.savefig(p,"out/r7_$(prec_v)_$(eps_v)_$(order)_$(ordprep)_$(n_tau)_epsilon_fct.pdf")
+                Plots.savefig(p,"out/r8_$(prec_v)_$(eps_v)_$(order)_$(ordprep)_$(n_tau)_epsilon_fct.pdf")
             end
             ind+= 1
         end
