16cdf288b0ebae660eac03b8af449502bacdedb1
Yves Mocquard <yves.mocquard@irisa.fr>
2020-03-27 08:52:53 +0100

    refining julia interface

--- src/interface.jl
+++ src/interface.jl
@@ -20,9 +20,9 @@ struct HiOscDEProblem{T} <:DiffEqBase.DEProblem
     epsilon::T
 end
 
-abstract type AbstractHiOscSolution{T,N,S} <: DiffEqBase.AbstractTimeseriesSolution{T,N,S} end
+abstract type AbstractHiOscSolution{T,N} <: DiffEqBase.AbstractTimeseriesSolution{T,N} end
 
-struct HiOscDESolution{T} <:AbstractHiOscSolution{T,T,T}
+struct HiOscDESolution{T} <:AbstractHiOscSolution{T,T}
     u::Vector{Vector{T}}
     sol_u_caret::Vector{Array{Complex{T},2}}
     t::Vector{T}
@@ -30,12 +30,14 @@ struct HiOscDESolution{T} <:AbstractHiOscSolution{T,T,T}
     order::Integer
     parphi::PreparePhi
     prob::HiOscDEProblem{T}
+    retcode
+    interp
     absprec
     relprec
 end
 function (sol::HiOscDESolution)(t)
     if sol.dense
-        return _getresult(sol.sol_u_caret, t, sol.parphi, t[1], t[end], sol.order)
+        return _getresult(sol.sol_u_caret, t, sol.parphi, sol.t[1], sol.t[end], sol.order)
     else
         return undef
     end
@@ -59,13 +61,14 @@ function DiffEqBase.solve(prob::HiOscDEProblem{T};
     nb_tau=32, order=4, order_prep=order+2, dense=true, 
     nb_t=100, getprecision=true
 ) where T<:AbstractFloat
+    retcode = :Success
     if getprecision
         s1=DiffEqBase.solve(prob; 
     nb_tau=nb_tau, order=order, order_prep=order_prep, dense=dense, nb_t=nb_t, getprecision=false)
         s2=DiffEqBase.solve(prob; 
-    nb_tau=nb_tau, order=order, order_prep=order_prep, dense=dense, nb_t=nb_t+1, getprecision=false)
-        absprec = norm(s1.sol[end] - s2.sol[end])
-        relprec = absprec/max( norm(s1.sol[end]), norm(s2.sol[end]))
+    nb_tau=nb_tau, order=order, order_prep=order_prep, dense=dense, nb_t=nb_t-1, getprecision=false)
+        absprec = norm(s1.u[end] - s2.u[end])
+        relprec = absprec/max( norm(s1.u[end]), norm(s2.u[end]))
         if dense
             for i = 1:10
                 t = rand(typeof(prob.epsilon)) * (prob.tspan[2]-prob.tspan[1]) 
@@ -77,23 +80,24 @@ function DiffEqBase.solve(prob::HiOscDEProblem{T};
                 relprec = max(relprec,rp)
             end
         end
-        return HiOscDESolution(s1.sol, s1.sol_u_caret, s1.t, dense, order,
-                s1.parphi, prob, absprec, relprec)
+        return HiOscDESolution(s1.u, s1.sol_u_caret, s1.t, dense, order,
+                s1.parphi, prob, retcode, nothing, absprec, relprec)
     end 
-    parphi = PreparePhi(prob.epsilon, nb_tau, prob.A, prob.f)
+    parphi = PreparePhi(prob.epsilon, nb_tau, prob.A, prob.f, t_0=prob.tspan[1])
     par_u0 = PrepareU0(parphi, order_prep, prob.u0)
-    pargen = PrepareTwoScalePureAB(nb_t, prob.tspan[2], order, par_u0;
-    t_begin=prob.tspan[1])
-    sol, sol_u_caret = if dense
+    pargen = PrepareTwoScalePureAB(nb_t, prob.tspan[2], order, par_u0)
+    sol, _, sol_u_caret = if dense
          twoscales_pure_ab(pargen, res_fft=true)
     else
-        twoscales_pure_ab(pargen), undef
+        twoscales_pure_ab(pargen), undef, undef
     end
-    sol = HiOscDESolution(sol, sol_u_caret, 
-        collect(0:nb_t)*prob.tspan[2]/big(nb_t)+prob.tspan[1],
+    return HiOscDESolution(
+        reshape(mapslices(x->[x], sol, dims=1),size(sol,2)), 
+        sol_u_caret, 
+        collect(0:nb_t)*prob.tspan[2]/big(nb_t).+prob.tspan[1],
         dense,
         order,
-        parphi, prob, undef, undef)
+        parphi, prob, retcode, nothing, undef, undef)
 end
 
 
