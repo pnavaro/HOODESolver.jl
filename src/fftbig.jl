d09f90bb6f3cbc84809071eaa944e612167eb247
Pierre Navaro <navaro@math.cnrs.fr>
2020-12-14 14:02:13 +0100

    Update fftbig.jl

--- src/fftbig.jl
+++ src/fftbig.jl
@@ -8,10 +8,10 @@ fft:
 using FFTW
 function _reverse_num(num, pos)
     result = 0
-    pos_m1 = pos-1
-    for i=0:pos_m1
-        if (num & (1 << i))  != 0
-           result |= 1 << (pos_m1 - i)
+    pos_m1 = pos - 1
+    for i = 0:pos_m1
+        if (num & (1 << i)) != 0
+            result |= 1 << (pos_m1 - i)
         end
     end
     return result
@@ -34,86 +34,86 @@ x is the type of non transformed data also called signal.
 
 """
 struct PrepareFftBig
-    size_fft
-    tab_permut
-    root_one
-    root_one_conj
+    size_fft::Any
+    tab_permut::Any
+    root_one::Any
+    root_one_conj::Any
     type::DataType
-    function PrepareFftBig( size_fft::Integer, x::T ) where {T<:AbstractFloat}
-        @assert prevpow(2,size_fft) == size_fft "size_fft=$size_fft is not a power of 2"
-        power = convert(Int64,log2(size_fft))
-        tab_permut = zeros(Int64,size_fft)
-        root_one = zeros(Complex{T}, size_fft )
-        root_one_conj = zeros(Complex{T}, size_fft )
-        prec= precision(T)
-        setprecision(prec+32) do
-            for i=1:size_fft
-                tab_permut[i] = _reverse_num( i-1, power) + 1
+    function PrepareFftBig(size_fft::Integer, x::T) where {T<:AbstractFloat}
+        @assert prevpow(2, size_fft) == size_fft "size_fft=$size_fft is not a power of 2"
+        power = convert(Int64, log2(size_fft))
+        tab_permut = zeros(Int64, size_fft)
+        root_one = zeros(Complex{T}, size_fft)
+        root_one_conj = zeros(Complex{T}, size_fft)
+        prec = precision(T)
+        setprecision(prec + 32) do
+            for i = 1:size_fft
+                tab_permut[i] = _reverse_num(i - 1, power) + 1
                 root_one[i] = round(
-    exp(one(T)*2big(pi)*im*(i-1)/size_fft),    
-    digits=prec+16, 
-    base=2 
-)
+                    exp(one(T) * 2big(pi) * im * (i - 1) / size_fft),
+                    digits = prec + 16,
+                    base = 2,
+                )
                 root_one_conj[i] = round(
-    exp(-one(T)*2big(pi)*im*(i-1)/size_fft),
-    digits=prec+16,
-    base=2
-)
+                    exp(-one(T) * 2big(pi) * im * (i - 1) / size_fft),
+                    digits = prec + 16,
+                    base = 2,
+                )
             end
         end
         return new(
-    size_fft, 
-    tab_permut, 
-    Complex{T}.(root_one), 
-    Complex{T}.(root_one_conj), 
-    typeof(x)
-)
+            size_fft,
+            tab_permut,
+            Complex{T}.(root_one),
+            Complex{T}.(root_one_conj),
+            typeof(x),
+        )
     end
 end
-PrepareFftBig( size_fft::Integer, type::DataType ) = PrepareFftBig( size_fft, one(type))
-PrepareFftBig( size_fft::Integer) = PrepareFftBig( size_fft, one(BigFloat))
-function fftbig!(par::PrepareFftBig, signal; flag_inv=false)
-    s=size(signal,2)
-    @assert prevpow(2,s) == s "size_fft(signal)=$s is not a power of 2"
-    s_div2 = div(s,2)
+PrepareFftBig(size_fft::Integer, type::DataType) = PrepareFftBig(size_fft, one(type))
+PrepareFftBig(size_fft::Integer) = PrepareFftBig(size_fft, one(BigFloat))
+function fftbig!(par::PrepareFftBig, signal; flag_inv = false)
+    s = size(signal, 2)
+    @assert prevpow(2, s) == s "size_fft(signal)=$s is not a power of 2"
+    s_div2 = div(s, 2)
     len = s
-    n_len = len>>1
+    n_len = len >> 1
     nb_r = 1
-    rootO = flag_inv ? par.root_one : par.root_one_conj;
-#    prec= precision(real(rootO[1]))
-#    setprecision(prec+32) do
-        while n_len != 0
-            start = 1
-            suite = start+n_len
-            for i=1:nb_r
-                 deb = 1
-                for j=deb:nb_r:s_div2
-                    signal[:,start], signal[:,suite] = (signal[:,start] + signal[:,suite,]), 
-        (signal[:,start] - signal[:,suite])*rootO[j]
-                    start += 1
-                    suite += 1
-                end
-                start = suite
-                suite = start+n_len
+    rootO = flag_inv ? par.root_one : par.root_one_conj
+    #    prec= precision(real(rootO[1]))
+    #    setprecision(prec+32) do
+    while n_len != 0
+        start = 1
+        suite = start + n_len
+        for i = 1:nb_r
+            deb = 1
+            for j = deb:nb_r:s_div2
+                signal[:, start], signal[:, suite] = (signal[:, start] + signal[:, suite]),
+                (signal[:, start] - signal[:, suite]) * rootO[j]
+                start += 1
+                suite += 1
             end
-            len = n_len
-            n_len >>= 1
-            nb_r <<= 1
+            start = suite
+            suite = start + n_len
         end
-#    end
-    signal .= signal[:,par.tab_permut]       
-    
+        len = n_len
+        n_len >>= 1
+        nb_r <<= 1
+    end
+    #    end
+    signal .= signal[:, par.tab_permut]
+
     if flag_inv
         signal ./= s
     end
     return signal
 end
-function fftbig(par::PrepareFftBig, signal; flag_inv=false)
+function fftbig(par::PrepareFftBig, signal; flag_inv = false)
     fl = flag_inv
     return fftbig!(
         par::PrepareFftBig,
         copy(convert(Array{Complex{par.type}}, signal)),
-        flag_inv=fl
+        flag_inv = fl,
     )
 end
 fftgen(_::Any, t::Array{Complex{Float64}}) = fft(t, (2,))
@@ -122,5 +122,7 @@ fftgen(p::PrepareFftBig, t::Array{T}) where {T<:AbstractFloat} = fftbig(p, t)
 fftgen(p::PrepareFftBig, t::Array{Complex{T}}) where {T<:AbstractFloat} = fftbig(p, t)
 ifftgen(_::Any, t::Array{Complex{Float64}}) = ifft(t, (2,))
 ifftgen(_::Any, t::Array{Float64}) = ifft(t, (2,))
-ifftgen(p::PrepareFftBig, t::Array{T}) where {T<:AbstractFloat}  = fftbig(p, t, flag_inv = true)
-ifftgen(p::PrepareFftBig, t::Array{Complex{T}}) where {T<:AbstractFloat}  = fftbig(p, t, flag_inv = true)
+ifftgen(p::PrepareFftBig, t::Array{T}) where {T<:AbstractFloat} =
+    fftbig(p, t, flag_inv = true)
+ifftgen(p::PrepareFftBig, t::Array{Complex{T}}) where {T<:AbstractFloat} =
+    fftbig(p, t, flag_inv = true)
